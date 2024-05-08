f22a225f3c3ab085f9256f40a7b903ab1eedaa2d
muhammadbinshahidtaimour <157716845+muhammadbinshahidtaimour@users.noreply.github.com>
2024-05-08 15:45:27 +0500

    ok

--- index.php
+++ index.php
@@ -107,7 +107,7 @@ include('header.php');
 					<div class="block1 wrap-pic-w">
 						<img height="250px" src="AdminPanel/img/<?php echo $cat['image'] ?>" alt="IMG-BANNER">
 
-						<a href="product.html" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
+						<a href="product.php?id=<?php echo $cat['id'] ?>" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
 							<div class="block1-txt-child1 flex-col-l">
 								<span class="block1-name ltext-102 trans-04 p-b-8">
 								<?php echo $cat['Name'] ?>
